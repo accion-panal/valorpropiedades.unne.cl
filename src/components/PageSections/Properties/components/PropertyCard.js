0b1163a4fc753d2bb0d34f07ab27956729a158eb
Fabian Salas <fabian.salas.astete@gmail.com>
2024-06-12 12:02:19 -0400

    fix

--- src/components/PageSections/Properties/components/PropertyCard.js
+++ src/components/PageSections/Properties/components/PropertyCard.js
@@ -41,7 +41,7 @@ const PropertyCard = ({ data, isList, property,valueUf }) => {
     // Validador de extension .jpg / .png/ .jpeg  para las imgs
     const validaImage = (image) => {
       if (image) {
-        const validExtensions = ['.jpg', '.jpeg', '.png'];
+        const validExtensions = ['.jpg','.JPG', '.jpeg', '.JPEG','.png', '.PNG', '.AVIF', '.avif'];
     
         if (validExtensions.some(ext => image.toLowerCase().endsWith(ext))) {
           return (
@@ -55,7 +55,7 @@ const PropertyCard = ({ data, isList, property,valueUf }) => {
                   ? 'h-[299px] w-[100%] md:w-[400px] xl:w-[400px] object-cover rounded-t-sm xl:rounded-none'
                   : 'rounded-t-sm'
               }  object-cover h-[299px] w-full p-4 xl:hover:scale-105 duration-300 xl:overflow-hidden`}
-              src={image}
+              src={image === 'https://accion.panal.house/images/Sin.png' ? 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg' : ''}
               alt={`top-img-${title}`}
               width="full"
               loading='lazy'
